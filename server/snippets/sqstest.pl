8c68316e29f816291732c74f97002fd6d92ee30c
Gonzalo Barco <gonzalo@computadorasyservice.com>
2011-08-05 09:58:33 -0300

    Original files

---
+++ server/snippets/sqstest.pl
@@ -0,0 +1,32 @@
+use strict;
+
+use Amazon::SQS::Simple;
+
+#AWS Private
+my $access_key = 'AKIAIC2DBRTIUKHMGASQ'; # Your AWS Access Key ID
+my $secret_key = '2Ofh3ICjeKpxeWBV2KGmKJ4co4WoeGtpumiiGEPX'; # Your AWS Secret Key
+
+#AWS Public
+my $public_access_key = '';
+my $public_secret_key = '';
+
+# Create an SQS object
+my $sqs = new Amazon::SQS::Simple($access_key, $secret_key);
+
+# Get Existing queue by endpoint
+my $q = $sqs->GetQueue('https://queue.amazonaws.com/041722291456/sinqrtel');
+
+# Retrieve a message
+my $msg = $q->ReceiveMessage( 'AttributeName.1' => 'All' );#, MaxNumberOfMessages=>1 );
+
+if ( defined $msg ) {
+  print $msg->MessageBody() . "\n";
+  if ( defined $msg->{Attribute} ) {
+	print "\t" . join("|", @{$msg->{Attribute}} );
+  }
+
+  # Delete the message
+  $q->DeleteMessage($msg->ReceiptHandle());
+} else {
+  print "No message visible right now\n";
+}
