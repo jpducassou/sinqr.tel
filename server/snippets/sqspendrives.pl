f346339ea844638480b908566ddfa9a483519555
unknown <windows7@windows7-PC.(none)>
2011-11-19 02:06:07 -0200

    Final 19/11 site. Pendrive game final.

---
+++ server/snippets/sqspendrives.pl
@@ -0,0 +1,33 @@
+use strict;
+
+use Amazon::SQS::Simple;
+
+#AWS Private
+my $access_key = 'AKIAIC2DBRTIUKHMGASQ'; # Your AWS Access Key ID
+my $secret_key = '2Ofh3ICjeKpxeWBV2KGmKJ4co4WoeGtpumiiGEPX'; # Your AWS Secret Key
+#my $queue_uri = 'https://queue.amazonaws.com/041722291456/sinqrtel_public'; #public queue uri
+my $queue_uri = 'https://queue.amazonaws.com/041722291456/sinqrtel_pendrives'; #public queue uri
+
+#AWS Public
+my $public_access_key = 'AKIAJZDRZUXVHSK3G6LA';
+my $public_secret_key = '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';
+
+# Create an SQS object
+my $sqs = new Amazon::SQS::Simple( $access_key, $secret_key );
+
+# Get Existing queue by endpoint
+my $q = $sqs->GetQueue( $queue_uri );
+
+# Retrieve a message
+while ( my @msg = $q->ReceiveMessage( 'AttributeName.1' => 'All' , MaxNumberOfMessages=>10, VisibilityTimeout=>60 ) ) {
+  foreach my $msg ( @msg ) {
+    print $msg->MessageBody() . "\n";
+    #if ( defined $msg->{Attribute} ) {
+    #  print "\t" . join("|", map {$_->{Name} . '=' . $_->{Value}} @{$msg->{Attribute}} );
+    #}
+    #  Delete the message
+    #unless ( $q->DeleteMessage($msg->ReceiptHandle()) ) {
+    #print "Delete failed\n";
+    #}
+  }
+}
