29261697c207a4e6483ff90db6d371aab60c1885
Jean Pierre Ducassou <jpducassou@gmail.com>
2012-03-08 22:40:25 -0200

    Scorebot gets dirty records.

--- server/bin/scorebot.pl
+++ server/bin/scorebot.pl
@@ -13,17 +13,46 @@ use Data::Dumper;
 use Amazon::SimpleDB::Client;
 use JSON::XS;
 use Amazon::S3;
+use Error qw(:try);
 
 # ============================================================================
+use Sinqrtel::SDB;
+
+# ============================================================================
+sub _attributes_to_hash {
+
+  my $attribute_list = shift;
+
+	my $attributes;
+	$attributes -> { $_ -> getName } = $_ -> getValue
+    for @$attribute_list;
+
+	return $attributes;
+
+}
+
 sub _select {
   my ($sdb, $select_expression, $next_token) = @_;
 
-	my $response = $sdb -> select({
-		SelectExpression                        => $select_expression,
-		($next_token        ? (NextToken        => $next_token)          : ()),
-	});
+warn 'query ' . $select_expression;
 
-	return $response -> getSelectResult -> getItem;
+	my $response;
+
+	eval {
+		$response = $sdb -> select({
+			SelectExpression                        => $select_expression,
+			($next_token        ? (NextToken        => $next_token)          : ()),
+		});
+		1;
+	} or do {
+		warn $@ -> getMessage();
+		return undef;
+	};
+
+	my $item_list = $response -> getSelectResult -> getItem;
+
+	my @result = map { [ $_ -> getName, _attributes_to_hash($_ -> getAttribute)  ]  } @$item_list;
+	return \@result;
 
 }
 
@@ -46,25 +75,28 @@ sub main {
 	my $aws_access_key = $config -> {'default.aws_access_key'}; # Your AWS Access Key ID
 	my $aws_secret_key = $config -> {'default.aws_secret_key'}; # Your AWS Secret Key
 
-	my $score_domain_name  = $config -> {'default.score_domain_name'};
+	my $sdb_domain_name    = $config -> {'default.sdb_domain_name'};
 	my $sdb_retrieve_limit = $config -> {'default.sdb_retrieve_limit'};
 
 	# ==========================================================================
 	# Get Simpledb handler
 	# ==========================================================================
-	my $sdb = new Amazon::SimpleDB::Client( $aws_access_key, $aws_secret_key );
+	my $sdb = new Amazon::SimpleDB::Client( $aws_access_key, $aws_secret_key ) || die 'Cannot create sdb client';
 
 	# ==========================================================================
 	# Get dirty record
 	# ==========================================================================
-	my $query = "select * from $score_domain_name where _dirty=\"1\" limit sdb_retrieve_limit";
+	my $query = "select * from $sdb_domain_name where _dirty=\"1\""; # limit sdb_retrieve_limit";
 	my $result_list = _select($sdb, $query);
 
 	foreach my $item (@$result_list) {
 		warn Dumper($item);
 
-		# Update simpledb as no _dirty
+		my $item_name;
+		my $old_timestamp;
 
+		# Update simpledb as no _dirty
+		# put_attributes_conditional($sdb, $sdb_domain_name, $item_name, { _dirty => 0 }, $old_timestamp);
 
 		# Update S3
 
