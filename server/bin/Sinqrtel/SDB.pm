8d4cd3963c270ce6fde2c5c56730942270632910
Gonzalo Barco <gbarcouy@gmail.com>
2012-03-29 18:16:41 -0300

    batch_delete_attributes a mitad de camino!

--- server/bin/Sinqrtel/SDB.pm
+++ server/bin/Sinqrtel/SDB.pm
@@ -127,6 +127,55 @@ sub select_attributes {
 
 }
 
+sub batch_delete_attributes {
+	my ( $sdb, $domain_name, $delete_items ) = @_;
+	#$delete_items = {
+	#	"item_name" => {attribute_name=>'attribute_value'}, #delete by condition
+	#	"item_name" => undef, #delete by item_name
+	#}
+	my $logger = Log::Log4perl -> get_logger();
+	
+	my $max_delete_items = 25; #documented at http://docs.amazonwebservices.com/AmazonSimpleDB/latest/DeveloperGuide/SDB_API_BatchDeleteAttributes.html
+	my $max_delete_attributes = 256; #documented at http://docs.amazonwebservices.com/AmazonSimpleDB/latest/DeveloperGuide/SDB_API_BatchDeleteAttributes.html
+	my @delete_items;
+	
+	my @request_parameters;
+	#do for all $delete_items
+	while ( keys %$delete_items > 0 ) {
+		#split in buckets of max allowed deletes or left keys
+		my $item_count = 1;
+		while ( $item_count <= $max_delete_items && keys %$delete_items > 0 ) {
+			my ( $item_name, $attributes ) = _hash_pop( $delete_items );
+			#documented docs.amazonwebservices.com/AmazonSimpleDB/latest/DeveloperGuide/SDB_API_DeleteAttributes.html#SDB_API_BatchPutAttributes_RequestParameters
+			push @request_parameters, 'Item.' . $item_count . 'ItemName=' . $item_name;
+			if ( defined $attributes ) {
+				$logger -> logdie("Too many parameters to an item_name on call to BatchDeleteAttributes for $item_name") if ( scalar keys %$attributes > $max_delete_attributes );
+				my $attribute_count = 1;
+				while ( keys %$attributes > 0 ) {
+					my ( $attribute_name, $attribute_value ) = _hash_pop( $attributes );
+					push @request_parameters, 'Item.' . $item_count . 'Attribute.' . $attribute_count . 'Name=' . $attribute_name;
+					push @request_parameters, 'Item.' . $item_count . 'Attribute.' . $attribute_count . 'Value=' . $attribute_value;
+					$attribute_count++;
+				}
+			}
+			$item_count++;
+		}
+		#make call!
+	}
+}
+
+sub _hash_pop {
+	my $hash = shift;
+	#fast get any key from hash
+	#documented http://www.perlmonks.org/?node_id=779281
+	scalar keys %$hash; #reset each in scalar context, very fast!
+	my ( $key, $value ) = each( %$hash ); #get one key and value from hash
+	
+	delete $hash->{ $key };
+	
+	return ( $key, $value );
+}
+
 # ============================================================================
 
 sub _hash_to_attributes {
