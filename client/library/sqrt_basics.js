760d39488970bd75aeb3ca1aa7a735672139a391
Gonzalo Barco <gbarcouy@gmail.com>
2012-05-23 17:08:06 -0300

    Javascript async load

--- client/library/sqrt_basics.js
+++ client/library/sqrt_basics.js
@@ -1,13 +1,4 @@
 //sqrt_basics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-24758076-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
 /*
 Define setObject on localStorage
 getObject = null if key not present instead of throwing Illegal access exception.
