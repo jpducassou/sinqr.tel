d0273b0c521776763b781ad312e35d5075215e59
Gonzalo Barco <gbarcouy@gmail.com>
2012-06-04 17:40:50 -0300

    Better sqrt_basics, async load complete.

--- client/library/sqrt_basics.js
+++ client/library/sqrt_basics.js
@@ -4,52 +4,54 @@ Define setObject on localStorage
 getObject = null if key not present instead of throwing Illegal access exception.
 */
 Storage.prototype.setObject = function(key, value) {
-    this.setItem(key, JSON.stringify(value));
-}
+  this.setItem(key, JSON.stringify(value));
+};
 
 Storage.prototype.getObject = function(key) {
-    var value = this.getItem(key);
-    if (value==undefined) {
-      return value;
-    } else {
-      //ERROR on undefined: "return value && JSON.parse(value);"
-      return JSON.parse(value);
-    }
-}
+	var value = this.getItem(key);
+	if (value==undefined) {
+		return value;
+	} else {
+		//ERROR on undefined: "return value && JSON.parse(value);"
+		return JSON.parse(value);
+	}
+};
 
-function sqrt_getSiteRoot() {
+var sqrt = {
+	getSiteRoot:function() {
     return "http://www.sinqrtel.com";
-}
-
-function sqrt_getSiteObjectInfoPath() {
+	},
+	getSiteObjectInfoPath:function() {
     return "/objectinfo/";
-}
+	},
+	getTimeStamp:function() {
+		return Math.round((new Date()).getTime() / 1000 );
+	},
+	sumAndUpdateScoresToTimeStamp:function( scoreArray, timeStamp ) {
+		var sum = 0;
+		var scoreIterator = 0;
+		while( scoreIterator < scoreArray.length ) {
+			if ( scoreArray[scoreIterator][0] > timeStamp ) {
+				sum+=scoreArray[scoreIterator][1];
+				scoreIterator++;
+			} else {
+				//delete one, iterator should not be incremented
+				scoreArray.splice(scoreIterator,1);
+			}
+		}
+		return sum;
+	},
+	getObjectInfo:function( objectName, success ) {
+		$.getJSON( sqrt_getSiteRoot() + sqrt_getSiteObjectInfoPath() + objectName, success );
+	},
+	addLocalScore:function( scoreArray, score, timeStamp ) {
+		scoreArray.push( [timeStamp, score] );
+	},
+	storeScore:function( scoreArray ) {
+		localStorage.setObject( 'score', scoreArray );
+	}
+};
 
-//function sqrt_sortTimeStamps(a,b) {return parseInt(a[0]) - parseInt(b[0]);}
-function sqrt_getTimeStamp() { return Math.round((new Date()).getTime() / 1000 ); };
-function sqrt_sumAndUpdateScoresToTimeStamp( scoreArray, timeStamp ) {
-  var sum = 0;
-  var scoreIterator = 0;
-  while( scoreIterator < scoreArray.length ) {
-    if ( scoreArray[scoreIterator][0] > timeStamp ) {
-      sum+=scoreArray[scoreIterator][1];
-      scoreIterator++;
-    } else {
-      //delete one, iterator should not be incremented
-      scoreArray.splice(scoreIterator,1);
-    }
-  }
-  return sum;
-}
-
-function sqrt_getObjectInfo( objectName, success ) {
-  $.getJSON( sqrt_getSiteRoot() + sqrt_getSiteObjectInfoPath() + objectName, success );
-}
-
-function sqrt_addLocalScore( scoreArray, score, timeStamp ) {
-  scoreArray.push( [timeStamp, score] );
-}
-
-function sqrt_storeScore( scoreArray ) {
-  localStorage.setObject( 'score', scoreArray );
+if (typeof window.sqrtAsyncCallback == 'function') {
+	window.sqrtAsyncCallback(this);
 }
