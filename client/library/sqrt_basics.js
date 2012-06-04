a65ffeeaa90afaece9494645c3406e019ed9bb3e
Gonzalo Barco <gonzalo@computadorasyservice.com>
2012-06-04 19:36:40 -0300

    sqrt_basics

--- client/library/sqrt_basics.js
+++ client/library/sqrt_basics.js
@@ -1,64 +1,43 @@
 //sqrt_basics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-24758076-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
 /*
 Define setObject on localStorage
 getObject = null if key not present instead of throwing Illegal access exception.
 */
-Storage.prototype.setObject = function(key, value) {
-    this.setItem(key, JSON.stringify(value));
-}
-
-Storage.prototype.getObject = function(key) {
-    var value = this.getItem(key);
-    if (value==undefined) {
-      return value;
-    } else {
-      //ERROR on undefined: "return value && JSON.parse(value);"
-      return JSON.parse(value);
-    }
-}
-
-function sqrt_getSiteRoot() {
+var sqrt = {
+  getSiteRoot:function() {
     return "http://www.sinqrtel.com";
-}
-
-function sqrt_getSiteObjectInfoPath() {
+  },
+  getSiteObjectInfoPath:function() {
     return "/objectinfo/";
-}
-
-//function sqrt_sortTimeStamps(a,b) {return parseInt(a[0]) - parseInt(b[0]);}
-function sqrt_getTimeStamp() { return Math.round((new Date()).getTime() / 1000 ); };
-function sqrt_sumAndUpdateScoresToTimeStamp( scoreArray, timeStamp ) {
-  var sum = 0;
-  var scoreIterator = 0;
-  while( scoreIterator < scoreArray.length ) {
-    if ( scoreArray[scoreIterator][0] > timeStamp ) {
-      sum+=scoreArray[scoreIterator][1];
-      scoreIterator++;
-    } else {
-      //delete one, iterator should not be incremented
-      scoreArray.splice(scoreIterator,1);
+  },
+  //function sqrt_sortTimeStamps(a,b) {return parseInt(a[0]) - parseInt(b[0]);}
+  getTimeStamp:function() {
+    return Math.round((new Date()).getTime() / 1000 );
+  },
+  sumAndUpdateScoresToTimeStamp:function( scoreArray, timeStamp ) {
+    var sum = 0;
+    var scoreIterator = 0;
+    while( scoreIterator < scoreArray.length ) {
+      if ( scoreArray[scoreIterator][0] > timeStamp ) {
+        sum+=scoreArray[scoreIterator][1];
+        scoreIterator++;
+      } else {
+        //delete one, iterator should not be incremented
+        scoreArray.splice(scoreIterator,1);
+      }
     }
-  }
-  return sum;
-}
-
-function sqrt_getObjectInfo( objectName, success ) {
-  $.getJSON( sqrt_getSiteRoot() + sqrt_getSiteObjectInfoPath() + objectName, success );
-}
-
-function sqrt_addLocalScore( scoreArray, score, timeStamp ) {
-  scoreArray.push( [timeStamp, score] );
-}
-
-function sqrt_storeScore( scoreArray ) {
-  localStorage.setObject( 'score', scoreArray );
+    return sum;
+  },
+  getObjectInfo:function( objectName, success ) {
+    $.getJSON( sqrt_getSiteRoot() + sqrt_getSiteObjectInfoPath() + objectName, success );
+  },
+  addLocalScore:function( scoreArray, score, timeStamp ) {
+    scoreArray.push( [timeStamp, score] );
+  },
+  storeScore:function( scoreArray ) {
+    localStorage.setItem('score', JSON.stringify(scoreArray ));
+  },
+  loadScore:function() {
+    return JSON.parse(this.getItem('score'));
+  },
 }
