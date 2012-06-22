5b5031ec3a2dfca1a8d06fecd7011d00183680d4
Gonzalo Barco <gbarcouy@gmail.com>
2012-06-22 17:08:22 -0300

    Google test! Better sqrt_test.js

--- www.sinqrtel.com/js/sqrt_basics.js
+++ www.sinqrtel.com/js/sqrt_basics.js
@@ -20,6 +20,11 @@ Storage.prototype.getObject = function(key) {
 var Sinqrtel = {
 	//configuration
 	sqrt_google_api_key:'AIzaSyAYDaIJG7v7j5mKy6cAjhzRI4LVa7yu6io',
+	sqrt_amazon_queue_uri:'https://queue.amazonaws.com/041722291456/sinqrtel_public',
+	sqrt_amazon_id:'AKIAIC2DBRTIUKHMGASQ',
+	sqrt_amazon_access_key:'2Ofh3ICjeKpxeWBV2KGmKJ4co4WoeGtpumiiGEPX',
+	sqrt_amazon_endpoint:'https://queue.amazonaws.com',
+	sqrt_amazon_version:'2009-02-01',
 	//sqrt_social_prefixes:{'facebook':'fb','twitter':'tw'},
 	//logic
 	sqrt_user_id:null,
@@ -194,6 +199,72 @@ var Sinqrtel = {
 				d.getElementById('sqrt_user_qr').src=this.getGooglQRUrl() + '.qr';
 			} catch (e) {};
 		}
+	},
+	sqs_send:function(message, stateChange) {
+		function _addZero(n) {
+				return ( n < 0 || n > 9 ? "" : "0" ) + n;
+		}
+		function _getNowTimeStamp() {
+			var time = new Date();
+			var gmtTime = new Date(time.getTime() + (time.getTimezoneOffset() * 60000));
+			
+			var d = [gmtTime.getFullYear(),gmtTime.getMonth()+1,gmtTime.getHours()];
+			var t = [gmtTime.getHours(), gmtTime.getMinutes(), gmtTime.getSeconds()];
+			
+			return d.join('-') + 'T' + t.join(':') + '.000Z';
+			
+			return (function(d){
+				return d.getFullYear()+'-'+_addZero()+'-'+_addZero(d.getDate())+'T'+_addZero(d.getHours())+':'+_addZero(d.getMinutes())+':'+_addZero(d.getSeconds())+ '.000Z';
+			})(gmtTime);
+		}
+		function _ignoreCaseSort(a, b) {
+			var ret = 0;
+			a = a.toLowerCase();
+			b = b.toLowerCase();
+			if(a > b) ret = 1;
+			if(a < b) ret = -1;
+			return ret;
+		}
+		function _getStringToSign( params ) {
+			var stringToSign = "";
+			
+			var fields = params.split("&");
+			fields.sort(_ignoreCaseSort);
+			for (var i = 0; i < fields.length; i++) {
+				var param = fields[i].split("=");
+				var name =  param[0];
+				var value = param[1];
+				//Parameters that should be skipped... but we should see none...
+				if (name == 'Signature' || undefined  == value) continue;
+				stringToSign += name;
+				stringToSign += decodeURIComponent(value);
+			}
+			return stringToSign;
+		}
+		function _generateSignedParams(actionName, messageBody, queueUrl, accessKeyId, secretKey, endpoint, version) {
+			//signable parameters
+			var params = "SignatureVersion=1&Action=" + actionName + "&Version=" + encodeURIComponent(version) +
+			"&QueueUrl=" + encodeURIComponent( queueUrl ) + "&MessageBody=" + encodeURIComponent( messageBody ) +
+			"&Timestamp=" + encodeURIComponent( getNowTimeStamp() ) + "&AWSAccessKeyId=" + encodeURIComponent(accessKeyId);
+		
+			//calculate V1 signature with signable parameters
+			var stringToSign = _getStringToSign( params );
+			var signature = Crypto.util.bytesToBase64( Crypto.HMAC (Crypto.SHA1, Crypto.charenc.UTF8.stringToBytes( stringToSign ), Crypto.charenc.UTF8.stringToBytes( secretKey ), {asBytes: true}));
+		
+			//parameters that should not be signed
+			params += "&Signature=" + encodeURIComponent( signature );
+			return params;
+		}
+		var postBody = _generateSignedParams( "SendMessage", message, Sinqrtel.sqrt_amazon_queue_uri, Sinqrtel.sqrt_amazon_id, Sinqrtel.sqrt_amazon_access_key, Sinqrtel.sqrt_amazon_endpoint, Sinqrtel.sqrt_amazon_version );
+		//request creation
+		var http = new XMLHttpRequest();
+		http.open("POST", Sinqrtel.sqrt_amazon_queue_uri, true);
+		http.setRequestHeader("Content-type", "application/x-www-form-urlencoded;"); //Send the proper header information along with the request
+		//request length can be skipped... so do it
+		//http.setRequestHeader("Content-length", url.length);
+		http.setRequestHeader("Connection", "close" );
+		http.onreadystatechange = stateChange;
+		http.send( postBody );
 	}
 };
 
